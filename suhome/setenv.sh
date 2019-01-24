baea07e110919a791e22b7555b9f3deb719e251b
rupert <rupert.lloyd+git@gmail.com>
2019-01-24 09:13:55 -0500

    set HOME as script dir

--- suhome/setenv.sh
+++ suhome/setenv.sh
@@ -1,5 +1,8 @@
+#!env sh
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 export PATH=/data/data/com.termux/files/usr/bin:/data/data/com.termux/files/usr/bin/applets:$PATH
 export LD_LIBRARY_PATH=/data/data/com.termux/files/usr/lib
-export HOME=/data/data/com.termux/files/home/suhome
+#export HOME=/data/data/com.termux/files/home/suhome
+export HOME=${DIR}
 export SHELL=$(which zsh)
 zsh
